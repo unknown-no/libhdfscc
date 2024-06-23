195d42f585b25a8c5800b9a40f79618207621894
westhide.dev <westhide.dev@gmail.com>
2024-06-23 15:38:06 +0000

    WIP

--- build.sh
+++ build.sh
@@ -3,13 +3,29 @@
 set -ex
 
 CURRENT_DIR=$(realpath $(dirname $0))
+INSTALL_DIR=${CURRENT_DIR}/.install
 
-cd ${CURRENT_DIR}
+function Init()
+{
+    echo "====Init===="
+    mkdir -p ${INSTALL_DIR}
+    mkdir -p ${CURRENT_DIR}/build
+}
 
-# rm -rf build
-mkdir -p build
+function Build()
+{
+    cd ${CURRENT_DIR}
 
-# cmake -S . -B build -DSANITIZE=address
-cmake -S . -B build
+    # cmake -S . -B build -DSANITIZE=address
+    cmake -S . -B build
+    cmake --build build
+}
 
-cmake --build build
+function Done()
+{
+    echo "====Done===="
+}
+
+Init
+Build
+Done
